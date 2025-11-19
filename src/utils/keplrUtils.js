1115ffbaaf3e2c4a4b0973518f80ea4a50a67db0
subversartscompany <subversarts.company@gmail.com>
2025-11-19 23:01:01 +0100

    Add connectKeplr function for wallet connection

--- src/utils/keplrUtils.js
+++ src/utils/keplrUtils.js
@@ -1,1 +1,12 @@
 
+
+export const connectKeplr = async (chainId = 'cosmos') => {
+  if (window.getOfflineSigner && window.getOfflineSigner(chainId)) {
+    const signer = window.getOfflineSigner(chainId);
+    await signer.connect();
+    const accounts = await signer.getAccounts();
+    return accounts[0].address;
+  } else {
+    throw new Error('Keplr wallet not installed or supported.');
+  }
+};
